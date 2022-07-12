2364ba7019cf58233d8471a03d5462917fb93f5e
Demon <kristyndcb@gmail.com>
2022-07-12 15:04:26 -0600

    code uploaded with restful api, starting with server-side auth.js to graphql conversion

---
+++ Develop/server/utils/auth.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+
+// set token secret and expiration date
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+module.exports = {
+  // function for our authenticated routes
+  //added {} to f
+  authMiddleware: function ({ req, res, next }) {
+    // allows token to be sent via  req.query or headers
+    //added req.body.token
+    let token = req.query.token || req.body.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: "You have no token!" });
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+      return res.status(400).json({ message: "invalid token!" });
+    }
+
+    // send to next endpoint
+    //do i need this here for graphql???
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
